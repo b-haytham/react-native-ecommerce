65da7f8438b87d05f9107b868569a3d27a72fff0
b-haytham <boussarsarhaytham@gmail.com>
2021-05-22 17:43:34 +0100

    setup redux

--- App.tsx
+++ App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Provider } from "react-redux";
 
 import Navigation from "./src/navigation";
+import store from "./src/redux/store";
 
 export default function App() {
-    return <Navigation />;
+    return (
+        <Provider store={store}>
+            <Navigation />
+        </Provider>
+    );
 }
