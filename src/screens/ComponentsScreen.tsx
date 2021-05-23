242bcffbb3f63e56f6a4ec327569fdede4c4de08
b-haytham <boussarsarhaytham@gmail.com>
2021-05-23 18:28:01 +0100

    create ScreenHeader component

--- src/screens/ComponentsScreen.tsx
+++ src/screens/ComponentsScreen.tsx
@@ -1,14 +1,19 @@
+import { MaterialIcons } from "@expo/vector-icons";
+import { useTheme } from "@shopify/restyle";
 import React from "react";
-import { View, StyleSheet, ScrollView } from "react-native";
+import { View, StyleSheet, ScrollView, TouchableOpacity } from "react-native";
 import Button from "../components/forms/form_elements/Button";
 import Input from "../components/forms/form_elements/Input";
 import Layout from "../components/Layout";
 import BottomTab from "../components/navigation/BottomTab";
+import Header from "../components/navigation/Header";
 import { Box, Text } from "../utils/restyle";
+import { Theme } from "../utils/theme";
 
 interface ComponentsScreenProps {}
 
 const ComponentsScreen: React.FC<ComponentsScreenProps> = ({}) => {
+    const theme = useTheme<Theme>();
     return (
         <Layout>
             <ScrollView style={styles.container}>
@@ -16,19 +21,44 @@ const ComponentsScreen: React.FC<ComponentsScreenProps> = ({}) => {
                     <Text variant="headline">Form Elements</Text>
                     <Input placeholder="Input" />
                     <Button
-                        title='PRIMARY'
-                        variant='PRIMARY'
+                        title="PRIMARY"
+                        variant="PRIMARY"
                         onPress={() => {}}
                     />
-                        <Button
-                        title='DEFAULT'
-                        variant='DEFAULT'
+                    <Button
+                        title="DEFAULT"
+                        variant="DEFAULT"
                         onPress={() => {}}
                     />
                 </Box>
-                <Box marginVertical='m'>
-                    <Text variant="headline" mb='m' marginHorizontal='m'>Navigation</Text>
-                    <BottomTab route_name='Home' />
+                <Box marginVertical="m">
+                    <Text variant="headline" mb="m" marginHorizontal="m">
+                        Navigation
+                    </Text>
+                    <BottomTab route_name="Home" />
+                    <Box marginVertical="s">
+                        <Header
+                            title="Header"
+                            left_icon={
+                                <TouchableOpacity>
+                                    <MaterialIcons
+                                        name="keyboard-arrow-left"
+                                        size={24}
+                                        color={theme.colors.black}
+                                    />
+                                </TouchableOpacity>
+                            }
+                            right_icon={
+                                <TouchableOpacity>
+                                    <MaterialIcons
+                                        name="search"
+                                        size={24}
+                                        color={theme.colors.black}
+                                    />
+                                </TouchableOpacity>
+                            }
+                        />
+                    </Box>
                 </Box>
             </ScrollView>
         </Layout>
