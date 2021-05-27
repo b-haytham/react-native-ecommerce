1f7e70c8ab3def75871ecfa058e9975a2c671c85
b-haytham <boussarsarhaytham@gmail.com>
2021-05-27 17:35:36 +0100

    create ListItem Component: ProfileScreen

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,24 +1,117 @@
-import React from 'react'
-import { } from 'react-native'
-import Layout from '../components/Layout'
-import BottomTab from '../components/navigation/BottomTab'
-import { ProfileScreenNavigationProps, ProfileScreenRouteProps } from '../navigation/ScreensNavigationRouteProps'
+import { Entypo, MaterialIcons } from "@expo/vector-icons";
+import { useTheme } from "@shopify/restyle";
+import React from "react";
+import { Dimensions, ScrollView, TouchableOpacity } from "react-native";
+import Layout from "../components/Layout";
+import ListItem from "../components/ListItem";
+import BottomTab from "../components/navigation/BottomTab";
+import {
+    ProfileScreenNavigationProps,
+    ProfileScreenRouteProps,
+} from "../navigation/ScreensNavigationRouteProps";
+import { Theme } from "../utils/theme";
 
-interface ProfileScreenProps{
-    navigation: ProfileScreenNavigationProps
-    route: ProfileScreenRouteProps
+interface ProfileScreenProps {
+    navigation: ProfileScreenNavigationProps;
+    route: ProfileScreenRouteProps;
 }
+const { width, height } = Dimensions.get("screen");
 
-const ProfileScreen: React.FC<ProfileScreenProps> = ({navigation, route}) =>{
+const ProfileScreen: React.FC<ProfileScreenProps> = ({ navigation, route }) => {
+    const theme = useTheme<Theme>();
     return (
         <Layout>
-            <BottomTab
-                route_name={route.name}
-                position='absolute'
-                bottom={0}
-            />
+            <BottomTab route_name={route.name} position="absolute" bottom={0} />
+            <ScrollView
+                style={{
+                    flex: 1,
+                    marginBottom: height * 0.1,
+                    paddingHorizontal: theme.spacing.m,
+                }}
+            >
+                <ListItem
+                    title="My Orders"
+                    description="Already have 4 Orders"
+                    left_icon={
+                        <Entypo
+                            name="shopping-bag"
+                            size={30}
+                            color={theme.colors.primary}
+                        />
+                    }
+                    right_icon={
+                        <TouchableOpacity>
+                            <MaterialIcons
+                                name="keyboard-arrow-right"
+                                size={30}
+                                color={theme.colors.primary}
+                            />
+                        </TouchableOpacity>
+                    }
+                />
+                <ListItem
+                    title="Shipping Addresses"
+                    description="Already have 2"
+                    left_icon={
+                        <Entypo
+                            name="address"
+                            size={30}
+                            color={theme.colors.primary}
+                        />
+                    }
+                    right_icon={
+                        <TouchableOpacity>
+                            <MaterialIcons
+                                name="keyboard-arrow-right"
+                                size={30}
+                                color={theme.colors.primary}
+                            />
+                        </TouchableOpacity>
+                    }
+                />
+                <ListItem
+                    title="My Reviews"
+                    description="Already have 7"
+                    left_icon={
+                        <MaterialIcons
+                            name="rate-review"
+                            size={30}
+                            color={theme.colors.primary}
+                        />
+                    }
+                    right_icon={
+                        <TouchableOpacity>
+                            <MaterialIcons
+                                name="keyboard-arrow-right"
+                                size={30}
+                                color={theme.colors.primary}
+                            />
+                        </TouchableOpacity>
+                    }
+                />
+                <ListItem
+                    title="Settings"
+                    description="Password, notifications ..."
+                    left_icon={
+                        <MaterialIcons
+                            name="settings"
+                            size={30}
+                            color={theme.colors.primary}
+                        />
+                    }
+                    right_icon={
+                        <TouchableOpacity>
+                            <MaterialIcons
+                                name="keyboard-arrow-right"
+                                size={30}
+                                color={theme.colors.primary}
+                            />
+                        </TouchableOpacity>
+                    }
+                />
+            </ScrollView>
         </Layout>
-    )
-}
+    );
+};
 
-export default ProfileScreen
+export default ProfileScreen;
