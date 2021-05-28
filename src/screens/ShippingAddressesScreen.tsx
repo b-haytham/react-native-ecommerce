f32f89849dda69b7647b37432e7905ae447b9ef3
b-haytham <boussarsarhaytham@gmail.com>
2021-05-28 14:01:25 +0100

    style Shipping Addresses Screen

--- src/screens/ShippingAddressesScreen.tsx
+++ src/screens/ShippingAddressesScreen.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import { Dimensions, ScrollView } from "react-native";
+
 import Layout from "../components/Layout";
 import BottomTab from "../components/navigation/BottomTab";
 import {
@@ -6,23 +8,96 @@ import {
     ShippingAdressesScreenRouteProps,
 } from "../navigation/ScreensNavigationRouteProps";
 
+import { useTheme } from "@shopify/restyle";
+import { Theme } from "../utils/theme";
+import Header from "../components/navigation/Header";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { Entypo, Ionicons } from "@expo/vector-icons";
+import { Box } from "../utils/restyle";
+
+import ShippingAddressCard from '../components/cards/ShippingAddressCard'
+
 interface ShippingAddressesScreenProps {
     navigation: ShippingAddressesScreenNavigationProps;
     route: ShippingAdressesScreenRouteProps;
 }
 
+const { width, height } = Dimensions.get("screen");
+
 const ShippingAddressesScreen: React.FC<ShippingAddressesScreenProps> = ({
     route,
     navigation,
 }) => {
+    const theme = useTheme<Theme>();
+    const [headerHeight, setHeaderHeight] = useState(0);
+
     return (
         <Layout>
+            <Header
+                onLayout={({
+                    nativeEvent: {
+                        layout: { width, height },
+                    },
+                }) => setHeaderHeight(height)}
+                elevation={2}
+                title="Shipping Addresses"
+                position="absolute"
+                top={0}
+                left_icon={
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate("Profile_Main")}
+                    >
+                        <Ionicons
+                            name="arrow-back"
+                            size={30}
+                            color={theme.colors.darkColor}
+                        />
+                    </TouchableOpacity>
+                }
+                right_icon={
+                    <TouchableOpacity
+                        
+                    >
+                        <Entypo
+                            name="plus"
+                            size={30}
+                            color={theme.colors.darkColor}
+                        />
+                    </TouchableOpacity>
+                }
+            />
             <BottomTab
                 elevation={5}
                 route_name={route.name}
                 position="absolute"
                 bottom={0}
             />
+              <ScrollView
+                style={{
+                    flex: 1,
+                    marginBottom: height * 0.1,
+                    marginTop: headerHeight - theme.spacing.l,
+                }}
+            >
+                <Box marginHorizontal="s">
+                        <ShippingAddressCard
+                            elevation={1}
+                            address="3 Newbridge Court, Chino Hills, CA 91709 United States"
+                            name="Jack Jack"
+                            is_checked_as_default={true}
+                            onCheckBoxChange={(v) => {}}
+                            onEditPress={() => {}}
+                        />
+                        <ShippingAddressCard
+                            elevation={1}
+                            address="3 Newbridge Court, Chino Hills, CA 91709 United States"
+                            name="Jack Jack"
+                            is_checked_as_default={false}
+                            onCheckBoxChange={(v) => {}}
+                            onEditPress={() => {}}
+                        />
+                    </Box>
+            </ScrollView>
         </Layout>
     );
 };
