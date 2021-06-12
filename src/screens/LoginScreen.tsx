5b7d9f1489dcc1d4f192f4f7393b544167a76676
boussarsar haytham <boussarsarhaytham@gmail.com>
2021-06-12 10:17:54 +0100

    style Login, Register Screen

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,25 +1,70 @@
-import React from 'react'
-import { StyleSheet } from 'react-native'
+import React from "react";
+import { TouchableOpacity } from "react-native";
 
-import { LoginScreenNavigationProps, LoginScreenRouteProps } from '../navigation/ScreensNavigationRouteProps'
-import { Text } from '../utils/restyle'
+import {
+    LoginScreenNavigationProps,
+    LoginScreenRouteProps,
+} from "../navigation/ScreensNavigationRouteProps";
+import { Box, Text } from "../utils/restyle";
 
-import Social from '../components/forms/form_elements/Social'
-import LoginForm from '../components/forms/LoginForm'
-import Layout from '../components/Layout'
-interface LoginScreenProps{
-    navigation: LoginScreenNavigationProps
-    route: LoginScreenRouteProps
+import Social from "../components/forms/form_elements/Social";
+import LoginForm from "../components/forms/LoginForm";
+import Layout from "../components/Layout";
+import { Image } from "react-native";
+interface LoginScreenProps {
+    navigation: LoginScreenNavigationProps;
+    route: LoginScreenRouteProps;
 }
 
-const LoginScreen: React.FC<LoginScreenProps> = ({navigation}) =>{
+const LoginScreen: React.FC<LoginScreenProps> = ({ navigation }) => {
     return (
-        <Layout p='l'>
-            <Text variant="headline" mt='xl' mb='xl'>Login</Text>
-            <LoginForm onSubmit={(data) => navigation.navigate('Main', {screen: 'Home'})} />
-            <Social title='Or Login with social account' />
+        <Layout no_padding>
+            <Box
+                padding="m"
+                bg="primary"
+                borderBottomLeftRadius="l"
+                borderBottomRightRadius="l"
+                elevation={10}
+                flexDirection="row"
+                justifyContent="space-between"
+                alignItems="center"
+            >
+                <Box>
+                    <Text variant="headline" mt="xl" color="white">
+                        Welcome
+                    </Text>
+                    <Text variant="description" mb="xl" color="white">
+                        Sign in to continue!
+                    </Text>
+                </Box>
+                <Box>
+                    <Image
+                        source={require("../../assets/icon.png")}
+                        style={{ width: 100, height: 100, borderRadius: 50 }}
+                    />
+                </Box>
+            </Box>
+            <Box paddingHorizontal="m" paddingTop="l">
+                <LoginForm
+                    onSubmit={(data) =>
+                        navigation.navigate("Main", { screen: "Home" })
+                    }
+                />
+                <Box
+                    flexDirection="row"
+                    justifyContent="center"
+                    alignItems="center"
+                >
+                    <Text variant="body">Don't Have an account?</Text>
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate("Register")}
+                    >
+                        <Text marginLeft='m' variant="body2" color='primary'>Sign up</Text>
+                    </TouchableOpacity>
+                </Box>
+            </Box>
         </Layout>
-    )
-}
+    );
+};
 
-export default LoginScreen
+export default LoginScreen;
--- src/screens/RegisterScreen.tsx
+++ src/screens/RegisterScreen.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Platform, ScrollView, TouchableOpacity } from "react-native";
+import { Dimensions, Image, KeyboardAvoidingView } from "react-native";
 import { StyleSheet } from "react-native";
 import Social from "../components/forms/form_elements/Social";
 import RegisterForm from "../components/forms/RegisterForm";
@@ -9,21 +11,75 @@ import {
     RegisterScreenNavigationProps,
     RegisterScreenRouteProps,
 } from "../navigation/ScreensNavigationRouteProps";
-import { Text } from "../utils/restyle";
+import { Box, Text } from "../utils/restyle";
 
 interface RegisterScreenProps {
     navigation: RegisterScreenNavigationProps;
     route: RegisterScreenRouteProps;
 }
 
+const { width, height } = Dimensions.get("screen");
+
 const RegisterScreen: React.FC<RegisterScreenProps> = ({ navigation }) => {
     return (
-        <Layout p="l">
-            <Text variant="headline" mt="xl" mb="xl">
-                Register
-            </Text>
-            <RegisterForm onSubmit={() => navigation.navigate("Login")} />
-            <Social title="Or Register with social account" />
+        <Layout no_padding>
+            <Box
+                padding="m"
+                bg="primary"
+                borderBottomLeftRadius="l"
+                borderBottomRightRadius="l"
+                elevation={10}
+                flexDirection="row"
+                justifyContent="space-between"
+                alignItems="center"
+            >
+                <Box maxWidth={width * 0.6}>
+                    <Text variant="headline" mt="xl" color="white">
+                        Create Account
+                    </Text>
+                    <Text variant="description" mb="xl" color="white">
+                        Sign up to get started!
+                    </Text>
+                </Box>
+                <Box>
+                    <Image
+                        source={require("../../assets/icon.png")}
+                        style={{ width: 100, height: 100, borderRadius: 50 }}
+                    />
+                </Box>
+            </Box>
+            <KeyboardAvoidingView
+                behavior={Platform.OS === "ios" ? "padding" : "height"}
+                style={{ flex: 1}}
+
+            >
+                <ScrollView>
+                    <Box padding="m">
+                        <RegisterForm
+                            onSubmit={() => navigation.navigate("Login")}
+                        />
+
+                        <Box
+                            flexDirection="row"
+                            justifyContent="center"
+                            alignItems="center"
+                        >
+                            <Text variant="body">Have an account?</Text>
+                            <TouchableOpacity
+                                onPress={() => navigation.navigate("Login")}
+                            >
+                                <Text
+                                    marginLeft="m"
+                                    variant="body2"
+                                    color="primary"
+                                >
+                                    Sign in
+                                </Text>
+                            </TouchableOpacity>
+                        </Box>
+                    </Box>
+                </ScrollView>
+            </KeyboardAvoidingView>
         </Layout>
     );
 };
